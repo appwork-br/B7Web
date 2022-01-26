33701bbd88e58da2283757050b7d1188ca03e0df
appwork-br <app_work@hotmail.com>
2022-01-26 14:21:32 -0300

    Aprendendo Nodejs

---
+++ Node/src/Matematica.ts
@@ -0,0 +1,13 @@
+export let versao: string = '1.0';
+
+export function somar(x: number , y: number): number {
+    return x + y;
+}
+
+export function subtrair(x: number , y: number): number {
+    return x - y;
+}
+
+export function multiplicar(x: number , y: number): number {
+    return x * y;
+}
---
+++ Node/src/index.ts
@@ -0,0 +1,9 @@
+import { somar, subtrair, multiplicar, versao} from './Matematica'
+
+let n1: number = 10;
+let n2: number = 2;
+
+console.log(`SOMA: ${somar(n1, n2)}`)
+console.log(`SUBTRAÇÃO: ${subtrair(n1, n2)}`)
+console.log(`MULTIPLICAÇÃO: ${multiplicar(n1, n2)}`)
+console.log(`VERSÃO: ${versao}`)
