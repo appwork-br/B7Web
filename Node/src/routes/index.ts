f47542023c504201beaaeb986de481f018a66595
appwork-br <app_work@hotmail.com>
2022-01-31 13:08:26 -0300

    Avançando

--- Node/src/routes/index.ts
+++ Node/src/routes/index.ts
@@ -3,19 +3,67 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response)=>{
-    res.send('Olá Mundo!');
+    let age: number = 38;
+    let showOld: boolean = false;
+
+    if(age > 50) {
+        showOld = true
+    }
+
+    res.render('pages/home', {
+        name: 'Daniel',
+        lastName: 'Oliveira',
+        showOld,
+        products: [
+            {title: 'Produto X', price: 10},
+            {title: 'Produto Y', price: 15},
+            {title: 'Produto W', price: 20}
+        ],
+        frasesDoDia: [
+            'Alguma coisa muito legal',
+            'Outra frase qualquer'
+        ]
+    });
 });
 
 router.get('/contato', (req: Request, res: Response)=>{
-    res.send('Formulário de Contato');
+    res.render('pages/contato');
 });
 
 router.get('/sobre', (req: Request, res: Response)=>{
-    res.send('Página institucional sobre a empresa');
+    res.render('pages/sobre');
 });
 
 router.get('/noticias', (req: Request, res: Response)=>{
     res.send('Pag de Noticias!');
+});
+
+router.get('/nome', (req: Request, res: Response) =>{
+    // console.log("query string",req.query.nome);
+    let nome: string = req.query.nome as string;
+    let idade: string = req.query.idade as string;
+
+    res.render('pages/nome', {
+        nome,
+        idade
+    });
+});
+
+router.get('/idade', (req: Request, res: Response) => {
+    let mostrarIdade: boolean = false;
+    let idade: number = 0;
+
+    if(req.query.ano) {
+        let anoNascimento: number = parseInt(req.query.ano as string);
+        let anoAtual: number = new Date().getFullYear();
+        idade = anoAtual - anoNascimento;
+        mostrarIdade = true;
+    }
+
+    res.render('pages/idade', {
+        idade,
+        mostrarIdade
+    });
 });
 
 export default router;
